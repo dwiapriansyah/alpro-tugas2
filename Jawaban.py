1e74a7445819ed304ce852de011eb3003d824b91
Dwi Apriansyah <115854672+dwiapriansyah@users.noreply.github.com>
2022-11-09 18:00:36 +0700

    Create Jawaban.py

---
+++ Jawaban.py
@@ -0,0 +1,35 @@
+#Tipe Data String
+String = "Hacker Belajar Python"
+print(String)
+ 
+#Tipe Data Integer
+Integer = 1500
+print(Integer)
+ 
+#Tipe Data Float
+Float = 99.123
+print(Float)
+ 
+#Tipe Data Complex Number
+foo = 4j
+print(foo)
+ 
+#Tipe Data Boolean
+Boolean = True
+print(Boolean)
+ 
+#Tipe Data List
+List = ["satu","dua","tiga","satu"]
+print(List)
+ 
+#Tipe Data Tuple
+Tuple = ("satu","dua","tiga","satu")
+print(Tuple)
+ 
+#Tipe Data Set
+Set = {"satu","dua","tiga","empat"}
+print(Set)
+ 
+#Tipe Data Dictionary
+Dictionary = {"satu":1, "dua":2.13, "tiga":"a", "empat": True}
+print(Dictionary)
